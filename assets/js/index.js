96f9e36150ca2997d40bd14c1cae8799d14dd88f
Claudiu <12200607+nclaudiuf@users.noreply.github.com>
2021-09-06 22:31:04 +0700

    added page css components

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,7 +16,7 @@
 import '../../node_modules/bootstrap/js/dist/modal.js';
 import '../../node_modules/bootstrap/js/dist/collapse.js';
 import '../../node_modules/bootstrap/js/dist/dropdown.js';
-import '../../scss/bs-config.scss';
+import '../../scss/theme.scss';
 import lozad from 'lozad';
 
 StyleModalCard();
