ed836bfd62596b1dee6370ff8387a5be45153550
Claudiu <12200607+nclaudiuf@users.noreply.github.com>
2021-09-06 20:16:01 +0700

    Initial commit

---
+++ purgecss.config.js
@@ -0,0 +1,11 @@
+/* 
+Bundle eveyrthing using Parcel and remove unused css using PurgeCSS 
+---------------Source & Credits ❤️ -------------------
+  PurgeCSS https://purgecss.com/configuration.html#configuration-file
+  Bootstrap Example: https://github.com/twbs/bootstrap-npm-starter
+*/
+
+module.exports = {
+	content: ['index.html', 'src/js/index.js'],
+	css: ['src/css/style.css']
+};
